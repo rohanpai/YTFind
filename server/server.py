54c806431f501d19248b13c35b9d461dfb5fbaa3
Rohan Pai <me@rohanpai.com>
2015-02-02 07:47:46 -0800

    Added json response for subtitles and created new link endpoint

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
-from flask import Flask, abort
+from flask import Flask, abort, jsonify
 from codecs import decode
 from YouTube.Subtitles import Subtitles
 
 import requests
+import xmltodict
 
 app = Flask(__name__)
 
@@ -10,8 +11,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/api/subtitles/<video_id>')
-def subtitles(video_id):
+@app.route('/api/subtitlesText/<video_id>')
+def subtitlesText(video_id):
+    subs = Subtitles()
+    subtitles = subs.get_subtitles(video_id)
+    subtitles_dict =  xmltodict.parse(str(subtitles))
+    return jsonify(subtitles_dict)
+
+@app.route('/api/subtitlesURL/<video_id>')
+def subtitlesURL(video_id):
     subs = Subtitles()
     subtitles = subs.get_subtitles_url(video_id)
     return subtitles
